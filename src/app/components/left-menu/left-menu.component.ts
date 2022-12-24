b9d3556fee499f8ada670aa002774edf02ba8584
money4nothing <money44nothing@gmail.com>
2022-12-24 10:29:43 +0100

    refactored menu items

--- src/app/components/left-menu/left-menu.component.ts
+++ src/app/components/left-menu/left-menu.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  label: string;
+  url: string;
+}
+
+const MENU_LIST: MenuItem[] = [
+  { label: 'Lista Foi', url: '/foi/list' },
+  { label: 'Lista CI per BTP', url: '/ci/list' },
+  { label: 'BTP Quotati', url: '/btp/list' },
+];
+
 @Component({
   selector: 'app-left-menu',
   templateUrl: './left-menu.component.html',
   styleUrls: ['./left-menu.component.scss']
 })
 export class LeftMenuComponent {
-
+  menuList = MENU_LIST;
 }
