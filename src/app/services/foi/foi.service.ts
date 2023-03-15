d87dbc8dd1982b9f426a20fbe9b08ae11924d7a3
money4nothing <money44nothing@gmail.com>
2023-03-15 09:11:05 +0100

    fixed value

--- src/app/services/foi/foi.service.ts
+++ src/app/services/foi/foi.service.ts
@@ -147,7 +147,7 @@ const FOI_EX_TABACCHI: readonly FoiExTabacchi[] = [
   { year: 2022, month: 10, value: 117.2 },
   { year: 2022, month: 11, value: 117.9 },
   { year: 2022, month: 12, value: 118.1 },
-  { year: 2023, month: 1, value: 118.2 },
+  { year: 2023, month: 1, value: 118.3 },
 ] as const;
 
 @Injectable({
